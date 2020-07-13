237d369eece7a9c05b6f4fcd271d45710e9a6de5
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-13 13:42:21 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,11 @@
+def greeting
+  puts "Hello World"
+end
+greeting
+greeting
+
+def multiply(num1, num2)
+  word = p "multiply got called with #{num1} and #{num2}"
+  word
+  num1 * num2
+end
---
+++ practice.rb
@@ -0,0 +1,12 @@
+english_music_by_city = {
+  :manchester => [
+    {
+      :band_name => "The Smiths",
+      :member_names => ["Morrissey", "Johnny", "Andy", "Mike"]
+    },
+    {
+      :band_name => "Joy Division",
+      :member_names => ["Peter", "Stephen", "Bernard", "Ian"]
+    },
+  ]
+}
